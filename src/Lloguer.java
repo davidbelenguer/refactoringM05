e5db0868a3c646eb9afbbf00b4fff7673b514624
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-05-06 22:52:06 +0200

    creacion de las tres clases

---
+++ src/Lloguer.java
@@ -0,0 +1,30 @@
+
+public class Lloguer {
+	private String data;
+	private int dies;
+	
+	public Lloguer(String data, int dies) {
+		this.data = data;
+		this.dies = dies;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public int getDies() {
+		return dies;
+	}
+
+	public void setDies(int dies) {
+		this.dies = dies;
+	}
+	
+	
+	
+
+}
