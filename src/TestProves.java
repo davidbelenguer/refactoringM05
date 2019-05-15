52b36865f0840b0f892f2dfdb5f670ff331f99f2
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-05-15 20:46:10 +0200

    intento de hacer el test

--- src/TestProves.java
+++ src/TestProves.java
@@ -6,10 +6,23 @@ public class TestProves {
 
 	@Test
 	public void Clientinforme() {
-		Client client = new Client("14789547B", "david", "666888774");
+		Client client1 = new Client("14789547B", "david", "666888774");
 		
-		double total = 0;
-		double quantitat = 0;
+				
+		double total = 14;
+		int bonificacions = 5;
+		double quantitat = 7;
+		
+		client1.setNom("David");
+		client1.getNom();
+		client1.getNif();
+		
+		String result = "";
+		String resultado = client1.informe();
+		/*quantitat +=3;
+		bonificacions +=10;
+		total += 14;*/
+		assertEquals(result,resultado);
 		
 		
 	}
